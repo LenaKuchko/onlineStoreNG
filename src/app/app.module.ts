501951b0a83b07bc4407c32d9f17e462b539c2bd
Olena Kuchko and Tyler Allen <olena+tyler@>
2017-07-12 11:45:23 -0700

    add Firebase database and edit/delete options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutPageComponent } from './about-page/about-page.component';
@@ -8,6 +12,15 @@ import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumComponent } from './album/album.component';
 import { routing } from './app.routing';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditAlbumComponent } from './edit-album/edit-album.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -17,10 +30,16 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     MarketplaceComponent,
     AlbumComponent,
     AlbumDetailComponent,
+    AdminComponent,
+    EditAlbumComponent,
   ],
   imports: [
     BrowserModule,
-    routing
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
